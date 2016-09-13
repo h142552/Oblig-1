ee8d3b5c2a67378d9b01a54575a93a4701e33c9c
Sigbjørn <sigmyh93@gmail.com>
2016-09-13 09:08:00 +0200

    Videre i oppgave 3

--- src/no/hib/DAT100/Oppgave3.java
+++ src/no/hib/DAT100/Oppgave3.java
@@ -7,15 +7,20 @@ public class Oppgave3 {
 		System.out.println("Skriv inn grunntal (reelt tall) og eksponent (heltall) separert med et mellomrom.");
 		In inn = new In();
 		
-		Integer x = Integer.parseInt(inn.next());
-		Integer n = Integer.parseInt(inn.next());
-		
-		System.out.println("Potenser med egen metode:  x^n = " + pow1(x, n));
-		System.out.println("Potenser med javas innebygde metode:  x^n = " + pow2(x, n));
+		try {
+			Integer x = Integer.parseInt(inn.next());
+			Integer n = Integer.parseInt(inn.next());
+			
+			System.out.println("Produkt med egen metode:  x^n = " + pow1(x, n));
+			System.out.println("Produkt med javas innebygde metode:  x^n = " + pow2(x, n));
 
-		if (pow1(x, n) == pow2(x, n))
-			System.out.println("Metodene returnerer samme svar. ");
-		
+			if (pow1(x, n) == pow2(x, n))
+				System.out.println("Metodene returnerer samme svar. ");
+		}
+		catch(Exception e){
+			System.out.println("Tall har feil format.");
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	public static double pow1(double x, int n){
